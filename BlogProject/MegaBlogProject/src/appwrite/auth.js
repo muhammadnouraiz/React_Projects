f39116d829674a677ee9264aa0e487faf3e0be6a
Muhammad Nouraiz <muhammadnouraiz842@gmail.com>
2025-12-24 20:47:13 +0500

    Appwrite Authorization

--- BlogProject/MegaBlogProject/src/appwrite/auth.js
+++ BlogProject/MegaBlogProject/src/appwrite/auth.js
@@ -35,10 +35,10 @@ export class AuthService {
 
     async login({ email, password }) {
         try {
-            return await this.account.createEmailPasswordSession(
+            return await this.account.createEmailPasswordSession({
                 email,
                 password
-        );
+        });
         } catch (error) {
             throw error;
         }
